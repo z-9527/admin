06c4e8a3059f160a589a6458d2f9663a6f3fe2fe
zhangZhiHao <137596665@qq.com>
2019-05-10 16:31:21 +0800

    完善页面顶部菜单

--- react/src/pages/Index/MyHeader.js
+++ react/src/pages/Index/MyHeader.js
@@ -1,13 +1,58 @@
 import React from 'react'
-import { Icon } from 'antd'
+import screenfull from 'screenfull'
+import { Icon, message, Menu,Avatar } from 'antd'
+import ColorPicker from '@/components/ColorPicker/index'
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class MyHeader extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const userTheme = JSON.parse(localStorage.getItem('user-theme'))
+        let color = '#1890ff'
+        if (userTheme) {
+            window.less.modifyVars(userTheme)
+            color = userTheme['@primary-color']
+        }
+        this.state = {
+            isFullscreen: false,
+            color: color,
+        }
+    }
     toggleCollapsed = () => {
         this.props.onChangeState({
             collapsed: !this.props.collapsed
         })
     }
+    toggleFullscreen = () => {
+        if (screenfull.enabled) {
+            screenfull.toggle().then(() => {
+                this.setState({
+                    isFullscreen: screenfull.isFullscreen
+                })
+            });
+        }
+    }
+    changeColor = (color) => {
+        window.less.modifyVars({
+            '@primary-color': color,
+        }).then(() => {
+            localStorage.setItem('user-theme', JSON.stringify({ '@primary-color': color }))
+            this.setState({
+                color
+            })
+            message.success('更换主题颜色成功')
+        })
+    }
+    resetColor = () => {
+        this.changeColor('#1890ff')
+    }
+
     render() {
+        const { isFullscreen, color } = this.state
+
         return (
             <div style={{ background: '#fff', padding: '0 16px' }}>
                 <Icon
@@ -15,8 +60,46 @@ class MyHeader extends React.Component {
                     type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
                     onClick={this.toggleCollapsed}
                 />
+                <div style={styles.headerRight}>
+                    <div style={styles.headerItem}>
+                        <ColorPicker color={color} onChange={this.changeColor} />
+                    </div>
+                    <div style={styles.headerItem}>
+                        <Menu mode="horizontal" selectable={false}>
+                            <SubMenu title={<div style={styles.avatarBox}><Avatar size='small' src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>&nbsp;<span>张致豪</span></div>}>
+                                <MenuItemGroup title="用户中心">
+                                    <Menu.Item key={1}><Icon type="user" />完善个人信息</Menu.Item>
+                                    <Menu.Item key={77}><Icon type="edit" />修改密码</Menu.Item>
+                                    <Menu.Item key={2}><Icon type="logout" />退出登录</Menu.Item>
+                                </MenuItemGroup>
+                                <MenuItemGroup title="设置中心">
+                                    <Menu.Item key={3} onClick={this.toggleFullscreen}><Icon type={isFullscreen ? 'fullscreen-exit' : 'fullscreen'} />切换全屏</Menu.Item>
+                                    <Menu.Item key={4} onClick={this.resetColor}><Icon type="ant-design" />恢复默认主题</Menu.Item>
+                                </MenuItemGroup>
+                            </SubMenu>
+                        </Menu>
+                    </div>
+                </div>
             </div>
         )
+    }
+}
+
+const styles = {
+    headerRight: {
+        float: 'right',
+        display: 'flex',
+        height: 64,
+        marginRight: 50
+    },
+    headerItem: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 20px'
+    },
+    avatarBox:{
+        display:'flex',
+        alignItems: 'center',
     }
 }
 
