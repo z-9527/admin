a1f09b92de874db4522f61e4e1152b852b25501a
zhangZhiHao <137596665@qq.com>
2019-05-14 14:59:10 +0800

    新增打字组件

---
+++ react/src/components/Typing/index.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+//如何将react节点转换为dom，比如拥有className、style、onClick的react元素，我们生成的dom如何保留这些
+
+class Typing extends React.Component {
+    static propTypes = {
+        delay: PropTypes.number,   //设置打印延时,单位为毫秒
+        frequency: PropTypes.number,   //设置打印频率
+        done: PropTypes.func    //打印结束的函数
+    }
+    static defaultProps = {
+        delay: 0,
+        frequency: 120,
+        done: () => { }
+    }
+
+    componentDidMount() {
+        this.chain = {          //此变量就是将要打印的对象
+            parent: null,
+            dom: this.wrapper,
+            val: []
+        };
+        this.chain.val = this._convert(this.props.children, this.chain.val)
+        setTimeout(() => {
+            this._play(this.chain)
+        }, this.props.delay)
+        console.log(this.props.children)
+    }
+    /**
+     * children转换为符合打印的数组
+     * @param {*} children  
+     * @param {array} arr 保存打印的数组
+     */
+    _convert(children, arr = []) {
+        let list = arr.slice()
+        if (Array.isArray(children)) {
+            for (let item of children) {
+                if (Object.prototype.toString.call(item) === '[object Object]') {        //元素节点
+                    let val = []
+                    let dom = this._createDom({
+                        ...item.props,
+                        type: item.type
+                    })
+                    val = this._convert(item.props.children, val)
+                    list.push({
+                        dom,
+                        val
+                    })
+                } else {                                                               //非元素节点
+                    if (item !== null && item !== undefined) {                         // 防止null、undefined这些变量
+                        list = list.concat(item.split(''))
+                    }
+                }
+            }
+        } else {
+            if (children !== null && children !== undefined) {                         // 防止null、undefined这些变量
+                list = list.concat(children.split(''))
+            }
+        }
+        return list
+    }
+    /**
+     * 打印字符
+     * @param {*} dom 父节点
+     * @param {*} val 打印内容
+     * @param {*} callback 打印完成的回调
+     */
+    _print(dom, val, callback) {
+        setTimeout(function () {
+            dom.appendChild(document.createTextNode(val));
+            callback();
+        }, this.props.frequency);
+    }
+    /**
+     * 打印节点
+     * @param {*} node 
+     */
+    _play = (node) => {
+        //当打印最后一个字符时，动画完毕，执行done
+        if (!node.val.length) {
+            if (node.parent) this._play(node.parent);
+            else this.props.done();
+            return;
+        }
+        let current = node.val.shift()    //获取第一个元素，并从打印列表中删除
+        if (typeof current === 'string') {
+            this._print(node.dom, current, () => {
+                this._play(node)
+            })
+        } else {
+            let dom = current.dom
+            node.dom.appendChild(dom)
+            this._play({
+                parent: node,
+                dom,
+                val: current.val
+            })
+        }
+    }
+    /**
+     * 根据信息生成dom节点
+     * @param {object} info 
+     */
+    _createDom(info) {
+        console.log(666, info)
+        let dom = null
+        dom = document.createElement(info.type)
+        return dom
+    }
+
+    render() {
+        return (
+            <div ref={el => this.wrapper = el}>
+
+            </div>
+        )
+    }
+}
+
+export default Typing
--- react/src/pages/Test/index.js
+++ react/src/pages/Test/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import {Card} from 'antd'
+import { Card } from 'antd'
+import Typing from '@/components/Typing'
 
-class Test extends React.Component{
-    render(){
+class Test extends React.Component {
+    state = {
+        name:'zzh'
+    }
+    test = ()=>{
+        console.log(1233)
+    }
+    render() {
         return (
             <Card>
-                fdasfsaf
-                <h2>fdsafaf</h2>
+                <Typing>
+                    fdsafadsf   
+                    打印带有标签的段落
+                    <p>我是段落</p>
+                    {null}
+                    {this.state.name}
+                    <ul className="tttt" style={{color:'red'}} onClick={this.test}>
+                        <li>列    表1</li>
+                        <li>列表2</li>
+                        <li>列表3</li>
+                    </ul>
+                </Typing>
             </Card>
         )
     }
