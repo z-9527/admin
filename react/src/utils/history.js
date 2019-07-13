4c6d7d5fc2053a9196613df6dffe72d3104381c5
zhangzhihao <137596665@qq.com>
2019-07-13 12:12:05 +0800

    完善项目

--- react/src/utils/history.js
+++ react/src/utils/history.js
@@ -1,3 +1,11 @@
 import { createBrowserHistory } from 'history'
 
-export default createBrowserHistory()
+
+const env = process.env.NODE_ENV  // 环境参数
+let options = {}
+if (env === 'production') {
+    options.basename = '/admin'    
+}
+
+
+export default createBrowserHistory(options)
