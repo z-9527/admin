0a3849ef1ddb4b4f1d9593d01e6b5a90b34ce51a
z-9527 <137596665@qq.com>
2021-01-23 18:31:02 +0800

    添加项目地址

--- react/src/pages/MobileUI/index.js
+++ react/src/pages/MobileUI/index.js
@@ -24,11 +24,13 @@ function MobileUI() {
     <Card bordered={false}>
       <div style={{ display: 'flex' }}>
         <div style={{ flex: 1, padding: 24 }} className="markdown">
-          <Typing>
+          <Typing style={{ height: 85 }}>
             代码和样式参考了
           <a href='https://github.com/ant-design/ant-design' target='_blank' rel="noopener noreferrer">antd</a>、
           <a href='https://github.com/youzan/vant' target='_blank' rel="noopener noreferrer">vant</a>、
-          react-components
+          react-components <br />
+            <a href='https://github.com/z-9527/sty-ui' target='_blank' rel="noopener noreferrer">项目地址</a> <br />
+            <a href='http://47.99.130.140/project/sty-ui/#/' target='_blank' rel="noopener noreferrer">预览地址</a>
           </Typing>
           <Row style={{ marginTop: 20 }}>
             {nav.slice(0, 2).map(item => <Item item={item} key={item.title} />)}
